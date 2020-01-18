91d2b562fa7e5425fdeff6df05119786235a9914
YukihiroMaekawa <nwojapan@gmail.com>
2020-01-18 16:44:26 +0900

    add gRPC-Server

--- cmd/go-lesson/main.go
+++ cmd/go-lesson/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+
+	"github.com/YukihiroMaekawa/go-lesson/internal/services"
+	"github.com/YukihiroMaekawa/go-lesson/pkg/customer"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
 
 func main() {
-	fmt.Printf("Hello World \n")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	paymentService := &services.CustomerService{}
+	customer.RegisterCustomerServiceServer(s, paymentService)
+
+	log.Printf("Listening on %v", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
