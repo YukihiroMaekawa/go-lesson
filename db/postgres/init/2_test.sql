2fb48589215c42d3c17aa23b7f128880e96db3ab
YukihiroMaekawa <nwojapan@gmail.com>
2020-01-21 12:40:08 +0900

    add docker

---
+++ db/postgres/init/2_test.sql
@@ -0,0 +1,10 @@
+create table if not exists customers2 (
+       id            bigserial          not null unique,
+       name          varchar(20)        not null,
+       address1      varchar(50)        not null,
+       address2      varchar(50)        not null,       
+       address3      varchar(50)        not null,
+       created_at    timestamp          not null,
+       updated_at    timestamp          not null,
+       primary key (id)
+);
